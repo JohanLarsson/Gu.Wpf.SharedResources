c15106d4fd1c421283cab3f65000545c9da251ec
Johan Larsson <johan_gavle1@hotmail.com>
2015-06-30 16:34:46 +0200

    Initial

---
+++ Controls/Keys.cs
@@ -0,0 +1,28 @@
+﻿namespace Controls
+{
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+
+    public static class Keys
+    {
+        private static readonly Dictionary<string, ComponentResourceKey> _keys = new Dictionary<string, ComponentResourceKey>(); 
+        public static readonly ComponentResourceKey RedBrushKey = new ComponentResourceKey(typeof(Keys), "RedBrushKey");
+
+        public static ComponentResourceKey GreenBrushKey
+        {
+            get { return Get(); }
+        }
+
+        private static ComponentResourceKey Get([CallerMemberName] string caller = null)
+        {
+            ComponentResourceKey key;
+            if (!_keys.TryGetValue(caller, out key))
+            {
+                key = new ComponentResourceKey(typeof(Keys),caller);
+                _keys.Add(caller, key);
+            }
+            return key;
+        }
+    }
+}
---
+++ Gu.Wpf.SharedResources/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Gu.Wpf.SharedResources")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gu.Wpf.SharedResources")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6bd8ff0f-5f51-41e8-b8eb-d135561b108a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Gu.Wpf.SharedResources")]
